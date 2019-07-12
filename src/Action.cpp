0a44ee6a3bd334a58afcb4c9ec2ed778e941cd76
Kehan.Xue <kehan.xue@gmail.com>
2019-07-12 23:33:16 +0800

    backup 201907122332

--- src/Action.cpp
+++ src/Action.cpp
@@ -3,8 +3,39 @@
 //
 
 #include "Action.h"
+#include "PIDController.h"
 
-int8_t vwpp::Action::run()
+vwpp::Velocity2D
+vwpp::Action::trackingLine(double_t _cur_line_x, double_t _cur_yaw, vwpp::Direction _direction,
+                           double_t _forward_vel)
+{
+    // TODO Get param from ros param serrver.
+    static PIDController pid_controller_x(1.0, 0.0, 1.0);
+    static PIDController pid_controller_y(1.0, 0.0, 1.0);
+    static PIDController pid_controller_yaw(1.0, 0.0, 1.0);
+
+
+
+    return vwpp::Velocity2D();
+}
+
+vwpp::VelocityZ vwpp::Action::adjustAltitude(double_t _target_altitude, double_t _cur_altitude)
+{
+    static PIDController pid_controller_z(1.0, 0.0, 1.0);
+    pid_controller_z.setTarget(_target_altitude);
+    pid_controller_z.update(_cur_altitude);
+
+    return pid_controller_z.output();
+}
+
+vwpp::Velocity2D vwpp::Action::hovering(double_t _cur_x, double_t _cur_y, double_t _cur_yaw)
+{
+    return vwpp::Velocity2D();
+}
+
+int8_t vwpp::Action::openClaw()
 {
     return 0;
 }
+
+
--- src/Action.h
+++ src/Action.h
@@ -6,26 +6,55 @@
 #define FIRA_ESI_ACTION_H_
 
 #include <cstdint>
+#include <vector>
+#include <cmath>
 
 namespace vwpp
 {
+
+    enum Direction
+    {
+        LOCAL_FORWARD = 0,
+        LOCAL_LEFT,
+        LOCAL_BACK,
+        LOCAL_RIGHT
+    };
+
     enum ActionID
     {
-        TrackingLine = 0,
-        AdjustAltitude,
-        Hovering,
-        OpenClaw,
-        CircularMotion
+        TRACKINGLINE = 0,
+        AUJUSTALTITUDE,
+        HOVERING,
+        OPENCLAW,
+        CIRCULARMOTION
     };
+
+    struct Velocity2D
+    {
+        double_t x;
+        double_t y;
+        double_t yaw;
+    };
+
+    typedef double_t VelocityZ;
 
     class Action
     {
     public:
-        int8_t run();
+
+        Velocity2D trackingLine(double_t _cur_line_x, double_t _cur_yaw, Direction _direction,
+                                double_t _forward_vel = 0.10);
+
+        VelocityZ adjustAltitude(double_t _target_altitude, double_t _cur_altitude);
+
+        Velocity2D hovering(double_t _cur_x, double_t _cur_y, double_t _cur_yaw);
+
+        int8_t openClaw();
 
         ActionID action_id;
     };
+
 }
 
-
 #endif //FIRA_ESI_ACTION_H_
+
